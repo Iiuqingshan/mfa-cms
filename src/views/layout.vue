20b23ace5f9e9c9252273ec4d816f63c6cbb4855
qingshan <liuqingshan86@gmail.com>
2024-09-14 11:16:29 +0800

    fix: aside border issue

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,6 +1,42 @@
 <template>
     <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+          <el-row>
+            <el-col>
+              <el-menu
+                default-active="2"
+                @open="handleOpen"
+                @close="handleClose"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b">
+                <el-submenu index="1">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>导航一</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                  </el-menu-item-group>
+                </el-submenu>
+                <el-menu-item index="2">
+                  <i class="el-icon-menu"></i>
+                  <span slot="title">导航二</span>
+                </el-menu-item>
+                <el-menu-item index="3">
+                  <i class="el-icon-document"></i>
+                  <span slot="title">导航三</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <i class="el-icon-setting"></i>
+                  <span slot="title">导航四</span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-aside>
         <el-container>
             <el-header>Header</el-header>
             <el-main>Main</el-main>
@@ -27,7 +63,7 @@ export default {
   }
   
   .el-aside {
-    background-color: #D3DCE6;
+    background-color: #545c64;
     color: #333;
     text-align: center;
     line-height: 200px;
@@ -38,5 +74,9 @@ export default {
     color: #333;
     text-align: center;
     line-height: 160px;
+  }
+
+  .el-menu {
+    border: none;
   }
 </style>
