e96eeadf76dcd8e3ef5a047f9b1b2dcb6412be45
qingshan <liuqingshan86@gmail.com>
2024-09-19 17:26:52 +0800

    feat: park search layout.

--- src/views/park/ParkList.vue
+++ src/views/park/ParkList.vue
@@ -3,8 +3,26 @@
   <div>
     <!-- 搜索 -->
     <div class="header">
-      <div class="search"></div>
-      <div class="group"></div>
+      <div class="search">
+        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+          <el-form-item label="审批人">
+            <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+          </el-form-item>
+          <el-form-item label="活动区域">
+            <el-select v-model="formInline.region" placeholder="活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">查询</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="group">
+        <el-button type="primary" icon="el-icon-edit"></el-button>
+        <el-button type="primary" icon="el-icon-share"></el-button>
+      </div>
     </div>
 
     <!-- 列表 -->
@@ -30,7 +48,11 @@
 export default {
   data() {
     return {
-      parks: []
+      parks: [],
+      formInline: {
+        user: '',
+        region: ''
+      }
     }
   },
   mounted() {
@@ -44,10 +66,19 @@ export default {
       } catch (err) {
         console.error('Error fetching data:', err);
       }
+    },
+
+    onSubmit() {
+      console.log('submit!');
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.search,
+.group {
+  text-align: left;
+  padding: 10px 10px;
+}
 </style>
